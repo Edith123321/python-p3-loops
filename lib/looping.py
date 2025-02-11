8a6843fe82e80e117fb5ea55ff2cbc8d968ba229
edith.githinji <edithgithinji2020@gmail.com>
2025-02-11 08:36:54 +0200

    for loop

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+   for i in range(10, -1, -1):
+       if i >= 0:
+           print(i)
+
+   print("Happy New Year!")
+    
+
+happy_new_year()
+       
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [i * i for i in int_list]
+       
+   
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):  # Loop from 1 to 100
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Run the function
+fizzbuzz()
+
